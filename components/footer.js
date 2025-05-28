2af04f2ebdabd7c694a66eeccdb58abebc4447a7
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-05-28 11:04:32 +0300

    Fix links

--- components/footer.js
+++ components/footer.js
@@ -10,7 +10,6 @@ const navigation = {
   pages: [
     { name: "Главная страница", href: "/" },
     { name: "Коллектив", href: "/team" },
-    // { name: "Новости", href: "/blog" },
     { name: "Контакты", href: "/contact" },
   ],
   legal: [
@@ -228,55 +227,10 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        {/* <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
-          <div>
-            <h3 className="text-sm/6 font-semibold text-gray-900">
-              Абонирайте се за нашия бюлетин
-            </h3>
-            <p className="mt-2 text-sm/6 text-gray-600">
-              Получавайте ценни правни съвети, анализи и актуални новини
-              директно във вашата поща.
-            </p>
-          </div>
-          {loading && (
-            <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 z-10 pointer-events-none">
-              <div className="w-12 h-12 border-4 border-gray-400 border-t-[#95161C] rounded-full animate-spin"></div>
-            </div>
-          )}
-          <form
-            onSubmit={handleSubmit}
-            className={`mt-6 sm:flex sm:max-w-md lg:mt-0 ${
-              loading ? "opacity-50 pointer-events-none" : ""
-            }`}
-          >
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email-address"
-              type="email"
-              required
-              placeholder="Въведете Вашия имейл"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full min-w-0 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:w-56 sm:text-sm/6"
-            />
-            <div className="mt-4 sm:mt-0 sm:ml-4 sm:shrink-0">
-              <button
-                type="submit"
-                className="flex w-full items-center justify-center rounded-md bg-[#95161C] hover:bg-gray-300 cursor-pointer hover:text-[#000000] px-3 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Абонирайте се
-              </button>
-            </div>
-          </form>
-        </div> */}
         <div className="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
           <div className="flex gap-x-6 md:order-2">
             {navigation.social.map((item) => (
-              <a
+              <Link
                 key={item.name}
                 href={item.href}
                 target={item.target}
@@ -285,19 +239,19 @@ export default function Footer() {
               >
                 <span className="sr-only">{item.name}</span>
                 <item.icon aria-hidden="true" className="size-6" />
-              </a>
+              </Link>
             ))}
           </div>
           <p className="mt-8 text-sm/6 text-gray-600 md:order-1 md:mt-0">
             &copy; {year} BRD, Inc. All rights reserved. Powered by{" "}
-            <a
+            <Link
               href="https://webstation.bg"
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-[#95161C]"
             >
               WebStation™
-            </a>
+            </Link>
           </p>
         </div>
       </div>
